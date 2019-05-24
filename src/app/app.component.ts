6ccbf54bfd200c399a7386c64c2362a22c08e985
Angéline <contact@angeline-toussaint.fr>
2019-05-24 13:38:11 +0200

    Add map in menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {Platform} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+    selector: 'app-root',
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Accueil',
-      url: '',
-      icon: 'home'
-    },
-    {
-      title: 'Calculatrice',
-      url: '/calc',
-      icon: 'calculator'
+    public appPages = [
+        {
+            title: 'Accueil',
+            url: '',
+            icon: 'home'
+        },
+        {
+            title: 'Calculatrice',
+            url: '/calc',
+            icon: 'calculator'
+        },
+        {
+            title: 'Carte',
+            url: '/map',
+            icon: 'map'
+        }
+    ];
+
+
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar
+    ) {
+        this.initializeApp();
     }
-  ];
 
-
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
