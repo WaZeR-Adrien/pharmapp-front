30dd21f2980bb61d674007866b098add426c39be
Angéline <contact@angeline-toussaint.fr>
2019-05-24 14:01:34 +0200

    Add map

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.page.html',
-  styleUrls: ['./map.page.scss'],
+    selector: 'app-map',
+    templateUrl: './map.page.html',
+    styleUrls: ['./map.page.scss'],
 })
+
 export class MapPage implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYW5nZWx0b3Vzc2FpbnQiLCJhIjoiY2p2cDBnemR6MXhnbjRhbnpvNGU0Ync2MCJ9.hSDPzS6Lh9ZJGSkalwBFRQ';
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11'
+        });
+    }
 }
