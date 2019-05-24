d9444a1dc26d94f474764b35b5e06edfa9d90790
Adrien <adrienm.martineau@gmail.com>
2019-05-24 08:58:58 +0200

    Fix issues

--- src/app/calc/calc.page.ts
+++ src/app/calc/calc.page.ts
@@ -6,34 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calc.page.scss'],
 })
 export class CalcPage implements OnInit {
-  achbrut:string;
-  achnet:string;
-  venteNet:string;
-  remise:string;
-  coeff:string;
-  result1:number;
-  result2:number;
-  result3:number;
-  result4:number;
+  achbrut: string;
+  achnet: string;
+  venteNet: string;
+  remise: string;
+  coeff: string;
+  result1: number;
+  result2: number;
+  result3: number;
+  result4: number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  tauxForm(){
-    this.result1 = (1-parseFloat(this.achnet)/parseFloat(this.achbrut))*100;
-  }
-  
-  prixAchnet(){
-    this.result2 = (parseFloat(this.achbrut)*(1-parseFloat(this.remise)));
+  tauxForm() {
+    this.result1 = (1 - parseFloat(this.achnet) / parseFloat(this.achbrut)) * 100;
   }
 
-  prixVentenet(){
-    this.result3 = (parseFloat(this.achnet)*parseFloat(this.coeff));
+  prixAchnet() {
+    this.result2 = (parseFloat(this.achbrut) * (1 - parseFloat(this.remise)));
   }
 
-  coeffMulti(){
-    this.result4 = (parseFloat(this.venteNet)*(1-parseFloat(this.achnet)));
+  prixVentenet() {
+    this.result3 = (parseFloat(this.achnet) * parseFloat(this.coeff));
+  }
+
+  coeffMulti() {
+    this.result4 = (parseFloat(this.venteNet) * (1 - parseFloat(this.achnet)));
   }
 }
