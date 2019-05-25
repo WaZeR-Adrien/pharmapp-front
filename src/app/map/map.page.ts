73457032639dc09def30812f69edc9ffb0ab5476
Adrien <adrienm.martineau@gmail.com>
2019-05-26 00:00:11 +0200

    Fix issue in contact service

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,29 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiClientService } from '../api-client/api-client.service';
-import { ContactService } from '../contact/contact.service';
-import { Contact } from '../contact/contact';
-import { getComponentDef } from '@angular/core/src/render3/definition';
-import { getOrCreateNodeInjectorForNode } from '@angular/core/src/render3/di';
+import {Component, OnInit} from '@angular/core';
+import {ApiClientService} from '../api-client/api-client.service';
+import {ContactService} from '../contact/contact.service';
+import {Personne} from '../people/personne';
 
 @Component({
-  selector: 'app-map',
-  templateUrl: './map.page.html',
-  styleUrls: ['./map.page.scss'],
+    selector: 'app-map',
+    templateUrl: './map.page.html',
+    styleUrls: ['./map.page.scss'],
 })
 export class MapPage implements OnInit {
-  contacts: Contact[];
+    contacts: Personne[];
 
-  constructor(private contactService: ContactService) { }
+    constructor(private contactService: ContactService) {
+    }
 
-  ngOnInit() {
-    this.contactService.getAll().subscribe(contacts =>{
-      
-        console.log(contacts);
-        contacts.forEach(function(element){
-
-        })
-    }, err => {
-
-    });
-  }
+    ngOnInit() {
+        this.contactService.getAll().subscribe(contacts => {
+            this.contacts = contacts;
+        });
+    }
 }
