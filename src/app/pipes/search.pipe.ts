f501f7473c2d6bab21686b621cacf13726309fc1
Adrien <adrienm.martineau@gmail.com>
2019-06-18 20:41:41 +0200

    Create search pipe

---
+++ src/app/pipes/search.pipe.ts
@@ -0,0 +1,17 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+    transform(items: any[], field: string, filter: string): any {
+
+        if (!items || !filter) {
+            return items;
+        }
+
+        return items.filter(item => item[field].toLowerCase().indexOf(filter.toLowerCase()) !== -1);
+    }
+
+}
