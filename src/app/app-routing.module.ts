d8b97b049d0bf411c109faa328ca02d5fcf2f016
Angéline <contact@angeline-toussaint.fr>
2019-06-18 09:35:27 +0200

    fix ,

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
     {path: '', loadChildren: './home/home.module#HomePageModule'},
     {path: 'calc', loadChildren: './calc/calc.module#CalcPageModule'},
-    {path: '', loadChildren: './contacts/contacts.module#ContactsPageModule'}
+    {path: '', loadChildren: './contacts/contacts.module#ContactsPageModule'},
     {path: 'stats', loadChildren: './stat/stat.module#StatPageModule'}
 ];
 
