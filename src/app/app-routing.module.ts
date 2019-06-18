5108a6f4cb3a7cea2589f3ab7c2cbbc7299d9993
Angéline <contact@angeline-toussaint.fr>
2019-06-18 09:34:50 +0200

    fix indentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
     {path: '', loadChildren: './home/home.module#HomePageModule'},
     {path: 'calc', loadChildren: './calc/calc.module#CalcPageModule'},
-    { path: '', loadChildren: './contacts/contacts.module#ContactsPageModule' }
-  { path: 'stats', loadChildren: './stat/stat.module#StatPageModule' }
+    {path: '', loadChildren: './contacts/contacts.module#ContactsPageModule'}
+    {path: 'stats', loadChildren: './stat/stat.module#StatPageModule'}
 ];
 
 @NgModule({
