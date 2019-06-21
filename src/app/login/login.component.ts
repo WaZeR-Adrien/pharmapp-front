1c643ee3a69c551af17563e7f6fb57cb5da35912
Adrien Martineau <officielghostfire@gmail.com>
2019-06-21 10:49:18 +0200

    Catch error

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../auth/auth.service';
 import {Globals} from '../globals';
+import {ToastController} from '@ionic/angular';
 
 @Component({
     selector: 'app-login',
@@ -12,18 +13,36 @@ export class LoginComponent implements OnInit {
     email: string;
     password: string;
 
-    constructor(private authService: AuthService, private globals: Globals) {
+    constructor(
+        private authService: AuthService,
+        private globals: Globals,
+        private toastController: ToastController
+    ) {
     }
 
     ngOnInit() {
     }
 
     login() {
-        this.authService.login(this.email, this.password).then(auth => {
-            localStorage.setItem('auth', JSON.stringify(auth));
-            this.globals.auth = auth;
-            delete this.email;
-            delete this.password;
+        this.authService.login(this.email, this.password)
+            .then(auth => {
+                localStorage.setItem('auth', JSON.stringify(auth));
+                this.globals.auth = auth;
+                delete this.email;
+                delete this.password;
+            })
+            .catch(err => {
+                if (err.error.message) {
+                    this.presentToast(err.error.message);
+                }
+            });
+    }
+
+    async presentToast(msg) {
+        const toast = await this.toastController.create({
+            message: msg,
+            duration: 2000
         });
+        toast.present();
     }
 }
