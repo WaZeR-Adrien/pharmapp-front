b9a3824f199ccc5a867e8a78875fafda87cd1d54
Adrien Martineau <officielghostfire@gmail.com>
2019-06-21 10:48:10 +0200

    Change handle error

--- src/app/api-client/api-client.service.ts
+++ src/app/api-client/api-client.service.ts
@@ -45,24 +45,10 @@ export class ApiClientService {
         }
 
         return observable.pipe(
-            tap(_ => console.log(method.toUpperCase() + ' ' + url + ' : success')),
-            catchError(this.handleError<any>())
+            tap(
+                _ => console.log(method.toUpperCase() + ' ' + url + ' : success'),
+                err => console.log(err.error)
+            )
         );
-    }
-
-    /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
-     */
-    private handleError<T>(result?: T) {
-        return (error: any): Observable<T> => {
-
-            console.error(error); // log to console instead
-
-            // Let the app keep running by returning an empty result.
-            return of(result as T);
-        };
     }
 }
